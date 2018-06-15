3397461495e507765e48aa745c5ffd0713e4d85a
Jan Michael Auer <jan.auer@sentry.io>
2018-06-15 15:40:32 +0200

    feat: Add a demo command

---
+++ src/commands/demo.ts
@@ -0,0 +1,53 @@
+import chalk from 'chalk';
+import { prompt, Question } from 'inquirer';
+import { Argv } from 'yargs';
+
+export const command = ['demo', 'd'];
+export const description = '🎬 Run a demo of craft';
+
+export const builder = (yargs: Argv) =>
+  yargs.option('m', {
+    alias: 'message',
+    description: 'Message to print when done',
+    type: 'string',
+  });
+
+/** Command line options. */
+interface DemoOptions {
+  message?: string;
+}
+
+/** Inquirer answers. */
+interface DemoAnswers {
+  message?: string;
+  ready: boolean;
+}
+
+export const handler = async (argv: DemoOptions) => {
+  const questions: Array<Question<DemoAnswers>> = [
+    {
+      message: 'What should I print in the end?',
+      name: 'message',
+      type: 'input',
+      when: () => !argv.message,
+    },
+    {
+      message: 'Ready?',
+      name: 'ready',
+      type: 'confirm',
+    },
+  ];
+
+  const answers = await prompt(questions);
+  console.info();
+
+  if (answers.ready) {
+    console.info(
+      '%s: %s',
+      chalk.green('success'),
+      chalk.dim(answers.message || argv.message || 'no message given')
+    );
+  } else {
+    console.info(chalk.yellow('maybe next time'));
+  }
+};
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import chalk from 'chalk';
+import * as yargs from 'yargs';
+
+// tslint:disable-next-line:no-unused-expression
+yargs
+  .commandDir('commands')
+  .demandCommand()
+  .version()
+  .alias('v', 'version')
+  .help()
+  .alias('h', 'help')
+  .strict()
+  .fail((message, _) => {
+    console.error('%s: %s', chalk.red('Error'), message);
+    console.error();
+    yargs.showHelp();
+  }).argv;
