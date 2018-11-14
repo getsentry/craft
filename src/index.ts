bfe470d7b087ee2c024a27f5e02a8b3c7a16ecee
Anton Ovchinnikov <anton@tonyo.info>
2018-11-14 11:39:51 +0100

    fix: Do not display redundant versions on "--version"

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,15 @@ import { checkForUpdates } from './utils/version';
 
 checkForUpdates();
 
-// tslint:disable-next-line:no-var-requires
-const pkg = require('../package.json');
-// Print the current version
-logger.info(`craft ${pkg.version}`);
+if (
+  process.argv.indexOf('-v') === -1 &&
+  process.argv.indexOf('--version') === -1
+) {
+  // tslint:disable-next-line:no-var-requires
+  const pkg = require('../package.json');
+  // Print the current version
+  logger.info(`craft ${pkg.version}`);
+}
 
 // tslint:disable-next-line:no-unused-expression
 yargs
