d09cb44b583b5b006851afa8c1b6cadd7360cd1d
Katie Byers <lobsterkatie@gmail.com>
2020-03-25 19:37:09 -0700

    preserve immutability of path templates

--- src/targets/gcs.ts
+++ src/targets/gcs.ts
@@ -164,22 +164,23 @@ export class GcsTarget extends BaseTarget {
   }
 
   /**
-   * Adds an interpolated bucket path, with `version` and `revision` filled
-   * in as appropriate, to the given PathTemplate object.
+   * Converts a PathTemplate into a BucketPath by filling in `version` and
+   * `revision` as appropriate.
    *
    * @param pathTemplate A path template with associated metadata
    * @param version The new version
    * @param revision The SHA revision of the new version
+   * @returns The resulting BucketPath object
    */
   private materializePathTemplate(
     pathTemplate: PathTemplate,
     version: string,
     revision: string
-  ): void {
-    const template = pathTemplate.template;
+  ): BucketPath {
+    const { template, metadata } = pathTemplate;
+
     if (!template) {
-      reportError(`Cannot insert data into path template ${template}`);
-      return;
+      throw new Error(`Invalid path template \`${template}\`!`);
     }
 
     let realPath = renderTemplateSafe(template.trim(), {
@@ -191,8 +192,10 @@ export class GcsTarget extends BaseTarget {
     if (realPath[0] !== '/') {
       realPath = `/${realPath}`;
     }
-    logger.debug(`Processed path template: ${template} and got ${realPath}`);
-    pathTemplate.path = realPath;
+    logger.debug(
+      `Processed path template: \`${template}\` and got \`${realPath}\``
+    );
+    return { path: realPath, metadata };
   }
 
   /**
@@ -229,13 +232,13 @@ export class GcsTarget extends BaseTarget {
     await forEachChained(
       this.targetConfig.pathTemplates,
       async (pathTemplate: PathTemplate): Promise<void> => {
-        // this adds a `path` property to the PathTemplate object, with
-        // `version` and `revision` values filled in
-        this.materializePathTemplate(pathTemplate, version, revision);
-        await this.gcsClient.uploadArtifacts(
-          localFilePaths,
-          pathTemplate as BucketPath
+        // fills `version` and `revision` values into the template
+        const bucketPath = this.materializePathTemplate(
+          pathTemplate,
+          version,
+          revision
         );
+        await this.gcsClient.uploadArtifacts(localFilePaths, bucketPath);
       }
     );
     logger.info('Upload to GCS complete.');
