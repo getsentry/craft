56f22013a7ac923c26d70b3057a7a74a21ddad63
Burak Yigit Kaya <byk@sentry.io>
2021-03-31 21:01:13 +0300

    feat(cli): Add a new targets command (#193)
    Adds a `targets` command to get a JSON list of available targets for a project. This is to be able to programmatically consume this informatino from places like getsentry/publish.

---
+++ src/commands/targets.ts
@@ -0,0 +1,18 @@
+import { Argv, CommandBuilder } from 'yargs';
+import { getConfiguration } from '../config';
+import { formatJson } from '../utils/strings';
+import { getAllTargetNames, getTargetId } from '../targets';
+
+export const builder: CommandBuilder = (yargs: Argv) => {
+  return yargs;
+};
+
+export function handler(): any {
+  const definedTargets = getConfiguration().targets || [];
+  const possibleTargetNames = new Set(getAllTargetNames());
+  const allowedTargetNames = definedTargets
+    .filter(target => target.name && possibleTargetNames.has(target.name))
+    .map(getTargetId);
+
+  console.log(formatJson(allowedTargetNames));
+}
