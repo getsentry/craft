c91c48b5ab35e67a89920d499f4dd34c14118e63
Burak Yigit Kaya <byk@sentry.io>
2021-04-30 22:36:13 +0300

    revert maxWorkers changes

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,4 @@ module.exports = {
   testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
-  maxWorkers: 1,
 };
