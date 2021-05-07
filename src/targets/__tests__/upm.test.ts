5f441b8efbcf2d6faad4050c442b30c83e04b10d
Burak Yigit Kaya <byk@sentry.io>
2021-05-07 15:08:56 +0300

    refine tests even more, add a sample publish test

--- src/targets/__tests__/upm.test.ts
+++ src/targets/__tests__/upm.test.ts
@@ -1,42 +1,66 @@
 import { NoneArtifactProvider } from '../../artifact_providers/none';
 import { UpmTarget } from '../upm';
 
-/** Returns a new UpmTarget test instance. */
-const getUpmTarget = () =>
-  new UpmTarget(
-    {
-      name: 'upm-test',
-      releaseOwner: 'getsentry-test',
-      releaseRepo: 'unity-test',
-    },
-    new NoneArtifactProvider(),
-    { owner: 'testSourceOwner', repo: 'testSourceRepo' }
-  );
-
-describe('artifacts', () => {
+describe('UPM Target', () => {
   const cleanEnv = { ...process.env };
+  let upmTarget: UpmTarget;
 
   beforeEach(() => {
     process.env = {
       ...cleanEnv,
       GITHUB_TOKEN: 'ghp_TAN21WJQkLtYVdHCh5eQJ8hTWoYvNh47gGWH',
+      DRY_RUN: '',
     };
     jest.resetAllMocks();
+
+    upmTarget = new UpmTarget(
+      {
+        name: 'upm-test',
+        releaseOwner: 'getsentry-test',
+        releaseRepo: 'unity-test',
+      },
+      new NoneArtifactProvider(),
+      { owner: 'testSourceOwner', repo: 'testSourceRepo' }
+    );
   });
 
-  test.each`
-    artifacts             | error
-    ${[]}                 | ${'Cannot publish UPM: No release artifact found.'}
-    ${['file1', 'file2']} | ${'Cannot publish UPM: Too many release artifacts found:file1\nfile2'}
-  `(
-    'error with artifact count $artifacts.length',
-    async ({ artifacts, error }) => {
-      const upmTarget = getUpmTarget();
-      upmTarget.getArtifactsForRevision = jest
-        .fn()
-        .mockReturnValueOnce(artifacts);
+  describe('artifacts', () => {
+    beforeEach(() => {
+      process.env.DRY_RUN = '';
+    });
+    test.each`
+      artifacts             | error
+      ${[]}                 | ${'Cannot publish UPM: No release artifact found.'}
+      ${['file1', 'file2']} | ${'Cannot publish UPM: Too many release artifacts found:file1\nfile2'}
+    `(
+      'error with artifact count $artifacts.length',
+      async ({ artifacts, error }) => {
+        upmTarget.getArtifactsForRevision = jest
+          .fn()
+          .mockResolvedValueOnce(artifacts);
 
-      expect(upmTarget.fetchArtifact('revision')).rejects.toThrow(error);
-    }
-  );
+        await expect(upmTarget.fetchArtifact('revision')).rejects.toThrow(
+          error
+        );
+      }
+    );
+  });
+
+  // TODO(byk): Add more tests for this
+  describe.skip('publish', () => {
+    beforeEach(() => {
+      upmTarget.fetchArtifact = jest
+        .fn()
+        .mockResolvedValueOnce({ filename: 'artifact.zip' });
+      upmTarget.artifactProvider.downloadArtifact = jest
+        .fn()
+        .mockResolvedValueOnce('some/test/path');
+    });
+
+    test('publish', () => {
+      return expect(
+        upmTarget.publish('version', 'revision')
+      ).resolves.not.toThrow();
+    });
+  });
 });
