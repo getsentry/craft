0c3c8950165d7b880fd9e10b9f7e9aafba656a6c
Burak Yigit Kaya <byk@sentry.io>
2021-05-10 22:54:30 +0300

    fix command description

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -2,7 +2,8 @@ import { getConfiguration, getGlobalGithubConfig } from '../config';
 import { formatJson } from '../utils/strings';
 
 export const command = ['config'];
-export const description = 'List defined targets as JSON array';
+export const description =
+  'Print the parsed, processed, and validated Craft config for the current project in pretty-JSON.';
 
 export async function handler(): Promise<void> {
   const github = await getGlobalGithubConfig();
