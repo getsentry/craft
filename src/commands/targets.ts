5030c50f8c37e81f39ce51c4a419ec787cc8f774
Burak Yigit Kaya <byk@sentry.io>
2021-05-20 11:18:29 +0300

    ref(targets): Move `getTargetId` to a static method on BaseTarget (#240)

--- src/commands/targets.ts
+++ src/commands/targets.ts
@@ -1,6 +1,7 @@
 import { getConfiguration } from '../config';
 import { formatJson } from '../utils/strings';
-import { getAllTargetNames, getTargetId } from '../targets';
+import { getAllTargetNames } from '../targets';
+import { BaseTarget } from '../targets/base';
 
 export const command = ['targets'];
 export const description = 'List defined targets as JSON array';
@@ -10,7 +11,7 @@ export function handler(): any {
   const possibleTargetNames = new Set(getAllTargetNames());
   const allowedTargetNames = definedTargets
     .filter(target => target.name && possibleTargetNames.has(target.name))
-    .map(getTargetId);
+    .map(BaseTarget.getId);
 
   console.log(formatJson(allowedTargetNames));
 }
