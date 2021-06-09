0edabdc3eb26afd682274f460f81320256a0fa8b
iker barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-06-09 18:18:54 +0200

    Collect required environment variables

--- src/targets/maven.ts
+++ src/targets/maven.ts
@@ -26,11 +26,30 @@ const USER_GRADLE_PROPS_FILE = join(homedir(), '/.gradle/gradle.properties');
 
 /** Config options for the "maven" target. */
 interface MavenTargetConfig {
-  ossrhUsername: string; // env var: OSSRH_USERNAME
-  ossrhPassword: string; // env var: OSSRH_PASSWORD
-  mavenUsername: string; // env var: MAVEN_CENTRAL_USERNAME
-  mavenPassword: string; // env var: MAVEN_CENTRAL_PASSWORD
+  // Required env variables
+  ossrhUsername: string; // OSSRH_USERNAME
+  ossrhPassword: string; // OSSRH_PASSWORD
+  mavenUsername: string; // MAVEN_CENTRAL_USERNAME
+  mavenPassword: string; // MAVEN_CENTRAL_PASSWORD
+  // Optional env variables (have a default value)
+  distributionsPath: string; // DISTRIBUTIONS_PATH
+  settingsPath: string; // SETTINGS_PATH
+  mavenRepoUrl: string; // MAVEN_REPO_URL
+  mavenRepoId: string; // MAVEN_REPO_ID
+  mavenCliPath: string; // MAVEN_CLI_PATH
+  gradleCliPath: string; // GRADLE_CLI_PATH
 }
+
+// Paths should be relative to the root of the repository
+const DEFAULT_ENV_VARIABLES: { [key: string]: string } = {
+  DISTRIBUTIONS_PATH: 'distributions/',
+  SETTINGS_PATH: 'scripts/settings.xml',
+  MAVEN_REPO_URL:
+    'https://oss.sonatype.org/service/local/staging/deploy/maven2/',
+  MAVEN_REPO_ID: 'ossrh',
+  MAVEN_CLI_PATH: 'scripts/mvnw.cmd',
+  GRADLE_CLI_PATH: 'gradlew',
+};
 
 /**
  * Target responsible for uploading files to Maven Central.
@@ -50,30 +69,44 @@ export class MavenTarget extends BaseTarget {
     artifactProvider: BaseArtifactProvider
   ) {
     super(config, artifactProvider);
-    // this.mavenConfig = this.getMavenConfig();
-    console.log('got maven config');
+    this.mavenConfig = this.getMavenConfig();
     this.github = getGithubClient();
     this.githubRemote = new GithubRemote(GIT_REPO_OWNER, GIT_REPO_NAME);
   }
 
   private getMavenConfig(): MavenTargetConfig {
     return {
+      // Required env variables
       ossrhUsername: this.getEnvVarValue('OSSRH_USERNAME'),
       ossrhPassword: this.getEnvVarValue('OSSRH_PASSWORD'),
-      // MAVEN_CENTRAL_* shouldnt be required if the user already has `gradle.properties`
+      // TODO: MAVEN_CENTRAL_* shouldnt be required if the user already has `gradle.properties`
       mavenUsername: this.getEnvVarValue('MAVEN_CENTRAL_USERNAME'),
       mavenPassword: this.getEnvVarValue('MAVEN_CENTRAL_PASSWORD'),
+      // Optional env variables
+      distributionsPath: this.getEnvVarOrDefault('DISTRIBUTIONS_PATH'),
+      settingsPath: this.getEnvVarOrDefault('SETTINGS_PATH'),
+      mavenRepoUrl: this.getEnvVarOrDefault('MAVEN_REPO_URL'),
+      mavenRepoId: this.getEnvVarOrDefault('MAVEN_REPO_ID'),
+      mavenCliPath: this.getEnvVarOrDefault('MAVEN_CLI_PATH'),
+      gradleCliPath: this.getEnvVarOrDefault('GRADLE_CLI_PATH'),
     };
   }
 
   private getEnvVarValue(envVar: string): string {
-    if (process.env['envVar']) {
-      return process.env['envVar'];
+    if (process.env[envVar]) {
+      return process.env[envVar] as string; // `as string` to make TS happy
     }
     throw new ConfigurationError(
       `Cannot publish to Maven Central: missing credentials.
       Please, use the ${envVar} environment variable.`
     );
+  }
+
+  private getEnvVarOrDefault(envVar: string): string {
+    if (process.env[envVar]) {
+      return process.env[envVar] as string; // `as string` to make TS happy
+    }
+    return DEFAULT_ENV_VARIABLES[envVar] as string;
   }
 
   public async publish(version: string, _revison: string): Promise<void> {
