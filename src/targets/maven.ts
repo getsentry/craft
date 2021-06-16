a0f6ff74fffe53c393c10b2c8091f9188e09506c
iker barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-06-16 13:38:36 +0200

    Move env vars to configuration and get config in an automated way

--- src/targets/maven.ts
+++ src/targets/maven.ts
@@ -34,37 +34,33 @@ const RETRY_DELAY_SECS = 3;
  */
 const RETRY_EXP_FACTOR = 2;
 
+const targetSecrets = [
+  'OSSRH_USERNAME',
+  'OSSRH_PASSWORD',
+  'MAVEN_CENTRAL_USERNAME',
+  'MAVEN_CENTRAL_PASSWORD',
+] as const;
+type SecretsType = typeof targetSecrets[number];
+
+const targetOptions = [
+  'androidDistDirPattern',
+  'androidFileReplaceePattern',
+  'androidFileReplacerStr',
+  'gradleCliPath',
+  'mavenCliPath',
+  'mavenSettingsPath',
+  'mavenRepoId',
+  'mavenRepoUrl',
+] as const;
+type OptionsType = typeof targetOptions[number];
+
+type TargetSettingType = SecretsType | OptionsType;
+
 /**
  * Config options for the "maven" target.
  */
 type MavenTargetConfig = {
-  [key in keyof (typeof RequiredConfig & typeof OptionalConfig)]: string;
-};
-
-enum RequiredConfig {
-  ossrhUsername = 'OSSRH_USERNAME',
-  ossrhPassword = 'OSSRH_PASSWORD',
-  mavenUsername = 'MAVEN_CENTRAL_USERNAME',
-  mavenPassword = 'MAVEN_CENTRAL_PASSWORD',
-}
-
-enum OptionalConfig {
-  distributionsPath = 'MAVEN_DISTRIBUTIONS_PATH',
-  settingsPath = 'MAVEN_SETTINGS_PATH',
-  mavenRepoUrl = 'MAVEN_REPO_URL',
-  mavenRepoId = 'MAVEN_REPO_ID',
-  mavenCliPath = 'MAVEN_CLI_PATH',
-  gradleCliPath = 'GRADLE_CLI_PATH',
-}
-
-const DEFAULT_ENV_VARIABLES: Record<string, string> = {
-  MAVEN_DISTRIBUTIONS_PATH: 'distributions/',
-  MAVEN_SETTINGS_PATH: 'scripts/settings.xml',
-  MAVEN_REPO_URL:
-    'https://oss.sonatype.org/service/local/staging/deploy/maven2/',
-  MAVEN_REPO_ID: 'ossrh',
-  MAVEN_CLI_PATH: 'scripts/mvnw.cmd',
-  GRADLE_CLI_PATH: 'gradlew',
+  [key in TargetSettingType]: string;
 };
 
 /**
@@ -81,7 +77,6 @@ export class MavenTarget extends BaseTarget {
     artifactProvider: BaseArtifactProvider
   ) {
     super(config, artifactProvider);
-    // TODO: check for config
     this.mavenConfig = this.getMavenConfig();
     this.checkRequiredSoftware();
     this.makeRegexpFromConfiguration();
@@ -96,39 +91,46 @@ export class MavenTarget extends BaseTarget {
    */
   private getMavenConfig(): MavenTargetConfig {
     return {
-      // Required env variables
-      ossrhUsername: this.getEnvVarValue(RequiredConfig.ossrhUsername),
-      ossrhPassword: this.getEnvVarValue(RequiredConfig.mavenPassword),
-      // TODO: MAVEN_CENTRAL_* shouldnt be required if the user already has `gradle.properties`
-      mavenUsername: this.getEnvVarValue(RequiredConfig.mavenUsername),
-      mavenPassword: this.getEnvVarValue(RequiredConfig.mavenPassword),
-      // Optional env variables
-      distributionsPath: this.getEnvVarOrDefault(
-        OptionalConfig.distributionsPath
-      ),
-      settingsPath: this.getEnvVarOrDefault(OptionalConfig.settingsPath),
-      mavenRepoUrl: this.getEnvVarOrDefault(OptionalConfig.mavenRepoUrl),
-      mavenRepoId: this.getEnvVarOrDefault(OptionalConfig.mavenRepoId),
-      mavenCliPath: this.getEnvVarOrDefault(OptionalConfig.mavenCliPath),
-      gradleCliPath: this.getEnvVarOrDefault(OptionalConfig.gradleCliPath),
+      ...this.getTargetSecrets(),
+      ...this.getTargetSettings(),
     };
   }
 
-  private getEnvVarValue(envVar: RequiredConfig): string {
-    if (process.env[envVar]) {
-      return process.env[envVar] as string; // `as string` to make TS happy
-    }
-    throw new ConfigurationError(
-      `Cannot publish to Maven Central: missing credentials.
-      Please, use the ${envVar} environment variable.`
-    );
+  private getTargetSecrets(): Record<TargetSettingType, string> {
+    const secrets = targetSecrets.map(secret => {
+      if (!process.env[secret]) {
+        throw new ConfigurationError(
+          `Cannot publish to Maven Central: missing credentials.
+            Please, use the ${secret} environment variable.`
+        );
+      }
+      return {
+        name: secret,
+        value: process.env[secret],
+      };
+    });
+    return this.reduceConfig(secrets);
   }
 
-  private getEnvVarOrDefault(envVar: OptionalConfig): string {
-    if (process.env[envVar]) {
-      return process.env[envVar] as string; // `as string` to make TS happy
-    }
-    return DEFAULT_ENV_VARIABLES[envVar];
+  private reduceConfig(
+    config: { name: string; value: string | undefined }[]
+  ): Record<string, string> {
+    return config.reduce((prev, current) => {
+      return {
+        ...prev,
+        [current.name]: current.value,
+      };
+    }, {});
+  }
+
+  private getTargetSettings(): Record<TargetSettingType, string> {
+    const settings = targetOptions.map(setting => {
+      return {
+        name: setting,
+        value: this.config[setting],
+      };
+    });
+    return this.reduceConfig(settings);
   }
 
   /**
@@ -185,8 +187,8 @@ export class MavenTarget extends BaseTarget {
       // first one to be indented to the right. To avoid that, these lines
       // shouldn't have that much space at the beginning, something the linter
       // doesn't agree with (and the code would be harder to read).
-      `mavenCentralUsername=${this.mavenConfig.mavenUsername}\n` +
-        `mavenCentralPassword=${this.mavenConfig.mavenPassword}`
+      `mavenCentralUsername=${this.mavenConfig.MAVEN_CENTRAL_USERNAME}\n` +
+        `mavenCentralPassword=${this.mavenConfig.MAVEN_CENTRAL_PASSWORD}`
     );
   }
 
@@ -287,7 +289,7 @@ export class MavenTarget extends BaseTarget {
           `-DpomFile=${pomFile}`,
           `-DrepositoryId=${this.mavenConfig.mavenRepoId}`,
           `-Durl=${this.mavenConfig.mavenRepoUrl}`,
-          `--settings ${this.mavenConfig.settingsPath}`,
+          `--settings ${this.mavenConfig.mavenSettingsPath}`,
         ]),
       'Uploading'
     );
