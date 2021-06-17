8468140ff94da1d44f976f5b89a029292a93ee94
iker barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-06-17 12:32:18 +0200

    Update tests for Maven target configuration

--- src/targets/__tests__/maven.test.ts
+++ src/targets/__tests__/maven.test.ts
@@ -1,30 +1,15 @@
 import { homedir } from 'os';
 import { join } from 'path';
+import { ConfigurationError } from '../../utils/errors';
 import { NoneArtifactProvider } from '../../artifact_providers/none';
 import { MavenTarget } from '../maven';
-
-const directories: Record<string, string[]> = {
-  android: ['androidChild'],
-  androidChild: ['android-release.aar'],
-
-  other: ['otherChild'],
-  otherChild: ['otherRelease'],
-};
 
 jest.mock('fs', () => ({
   ...jest.requireActual('fs'),
   promises: {
-    readdir: async (parentDir: string) => directories[parentDir],
     writeFile: async () => {
       /* do nothing */
     },
-  },
-}));
-
-jest.mock('child_process', () => ({
-  ...jest.requireActual('child_process'),
-  exec: () => {
-    /** do nothing */
   },
 }));
 
@@ -35,199 +20,93 @@ jest.mock('../../utils/system', () => ({
   },
 }));
 
-const REQUIRED_OPTIONS: string[] = [
-  'GITHUB_TOKEN', // Not specific to Maven target
+const targetSecrets: string[] = [
   'OSSRH_USERNAME',
   'OSSRH_PASSWORD',
   'MAVEN_CENTRAL_USERNAME',
   'MAVEN_CENTRAL_PASSWORD',
 ];
 
-const OPTIONAL_OPTIONS: string[] = [
-  'MAVEN_DISTRIBUTIONS_PATH',
-  'MAVEN_SETTINGS_PATH',
-  'MAVEN_REPO_URL',
-  'MAVEN_REPO_ID',
-  'MAVEN_CLI_PATH',
-  'GRADLE_CLI_PATH',
+const targetOptions: string[] = [
+  'androidDistDirPattern',
+  'androidFileReplaceePattern',
+  'androidFileReplacerStr',
+  'gradleCliPath',
+  'mavenCliPath',
+  'mavenSettingsPath',
+  'mavenRepoId',
+  'mavenRepoUrl',
 ];
 
 const DEFAULT_OPTION_VALUE = 'my_default_value';
 
-function setRequiredConfig(): void {
-  for (const option of REQUIRED_OPTIONS) {
+interface TestTargetConfig {
+  name: string;
+  [otherKeys: string]: string;
+}
+
+function setTargetSecretsInEnv(): void {
+  for (const option of targetSecrets) {
     process.env[option] = DEFAULT_OPTION_VALUE;
   }
 }
 
-function removeRequiredConfig(): void {
-  for (const option of REQUIRED_OPTIONS) {
+function removeTargetSecretsFromEnv(): void {
+  for (const option of targetSecrets) {
     delete process.env[option];
   }
 }
 
-function setOptionalConfig(): void {
-  for (const option of OPTIONAL_OPTIONS) {
-    process.env[option] = DEFAULT_OPTION_VALUE;
-  }
+function getTargetOptions(): TestTargetConfig {
+  const defaultOptions: TestTargetConfig = {
+    name: 'maven',
+  };
+  targetOptions.map(option => (defaultOptions[option] = DEFAULT_OPTION_VALUE));
+  return defaultOptions;
 }
 
-function removeOptionalConfig(): void {
-  for (const option of OPTIONAL_OPTIONS) {
-    delete process.env[option];
-  }
-}
-
-function createMavenTarget(): MavenTarget {
-  return new MavenTarget(
-    {
-      name: 'maven',
-      ['testKey']: 'testValue',
-    },
-    new NoneArtifactProvider()
-  );
+function createMavenTarget(targetOptions?: TestTargetConfig): MavenTarget {
+  const options = targetOptions
+    ? targetOptions
+    : {
+        name: 'maven',
+        ['testKey']: 'testValue',
+      };
+  return new MavenTarget(options, new NoneArtifactProvider());
 }
 
 describe('maven target configuration', () => {
-  beforeEach(() => {
-    removeRequiredConfig();
-    removeOptionalConfig();
-  });
+  beforeEach(() => removeTargetSecretsFromEnv());
 
-  describe('required options exist', () => {
-    test('with optional options', () => {
-      setRequiredConfig();
-      setOptionalConfig();
-      createMavenTarget();
-    });
-    test('without optional options', () => {
-      setRequiredConfig();
-      createMavenTarget();
-    });
-  });
-
-  describe("required options don't exist", () => {
-    test('with optional options', () => {
-      setOptionalConfig();
-      expect(createMavenTarget).toThrowError();
-    });
-    test('without optional options', () => {
-      expect(createMavenTarget).toThrowError();
-    });
-  });
-});
-
-describe('upload to Maven', () => {
-  beforeAll(() => {
-    setRequiredConfig();
-  });
-
-  afterAll(() => {
-    removeRequiredConfig();
-  });
-
-  function getFileParameters(
-    distDir: string,
-    moduleName: string
-  ): Record<string, string> {
-    return {
-      javadocFile: join(distDir, `${moduleName}-javadoc.jar`),
-      sourcesFile: join(distDir, `${moduleName}-sources.jar`),
-      pomFile: join(distDir, 'pom-default.xml'),
-    };
+  function getExpectedValueOfConfigKey(configKey: string): string | RegExp {
+    if (
+      configKey.includes('androidDistDirPattern') ||
+      configKey.includes('androidFileSearchPattern')
+    ) {
+      return new RegExp(DEFAULT_OPTION_VALUE);
+    }
+    return DEFAULT_OPTION_VALUE;
   }
 
-  test("when it's an Android distribution", async () => {
-    const parentDir = 'android';
-    process.env.MAVEN_DISTRIBUTIONS_PATH = parentDir;
-    const distDir = directories[parentDir][0];
-    const androidTestFile = directories[distDir][0];
-    const { javadocFile, sourcesFile, pomFile } = getFileParameters(
-      distDir,
-      distDir
-    );
-
-    const mavenTarget = createMavenTarget();
-    const mavenUploadCmdSpy = jest.spyOn(mavenTarget, 'getMavenUploadCmd');
-    const androidDisSpy = jest.spyOn(mavenTarget, 'getAndroidDistributionFile');
-    await mavenTarget.upload();
-
-    expect(androidDisSpy).toHaveBeenCalledTimes(1);
-    expect(mavenUploadCmdSpy).toHaveBeenCalledTimes(1);
-    expect(mavenUploadCmdSpy).toHaveBeenCalledWith(
-      androidTestFile,
-      javadocFile,
-      sourcesFile,
-      pomFile
+  test('with target secrets and options', () => {
+    setTargetSecretsInEnv();
+    const mvnTarget = createMavenTarget(getTargetOptions());
+    targetOptions.map(secret =>
+      expect(mvnTarget.config).toEqual(
+        expect.objectContaining({
+          [secret]: getExpectedValueOfConfigKey(secret),
+        })
+      )
     );
   });
 
-  test("when it's not an Android distribution", async () => {
-    const parentDir = 'other';
-    process.env.MAVEN_DISTRIBUTIONS_PATH = parentDir;
-    const distDir = directories[parentDir][0];
-    const nonAndroidTestFile = join(distDir, `${distDir}.jar`);
-    const { javadocFile, sourcesFile, pomFile } = getFileParameters(
-      distDir,
-      distDir
-    );
-
-    const mavenTarget = createMavenTarget();
-    const mavenUploadCmdSpy = jest.spyOn(mavenTarget, 'getMavenUploadCmd');
-    const androidDisSpy = jest.spyOn(mavenTarget, 'getAndroidDistributionFile');
-    await mavenTarget.upload();
-
-    expect(androidDisSpy).toHaveBeenCalledTimes(1);
-    expect(mavenUploadCmdSpy).toHaveBeenCalledTimes(1);
-    expect(mavenUploadCmdSpy).toHaveBeenCalledWith(
-      nonAndroidTestFile,
-      javadocFile,
-      sourcesFile,
-      pomFile
-    );
-  });
-});
-
-describe('get Android distribution file', () => {
-  beforeAll(() => {
-    setRequiredConfig();
+  test('with only target secrets', () => {
+    setTargetSecretsInEnv();
+    expect(createMavenTarget).toThrowError(ConfigurationError);
   });
 
-  afterAll(() => {
-    removeRequiredConfig();
-  });
-
-  test('when exists', async () => {
-    const parentDirectory = 'android';
-    process.env.MAVEN_DISTRIBUTIONS_PATH = parentDirectory;
-    const expectedChildDirectory = directories[parentDirectory][0];
-    const expectedAndroidFile = directories[expectedChildDirectory][0];
-
-    const mavenTarget = createMavenTarget();
-    const androidDisSpy = jest.spyOn(mavenTarget, 'getAndroidDistributionFile');
-    await mavenTarget.upload();
-
-    expect(androidDisSpy).toHaveBeenCalledTimes(1);
-    expect(androidDisSpy).toHaveBeenCalledWith(expectedChildDirectory);
-    expect(androidDisSpy).toHaveReturnedWith(
-      Promise.resolve(expectedAndroidFile)
-    );
-
-    jest.resetAllMocks();
-  });
-
-  test("when it doesn't exist", async () => {
-    const parentDirectory = 'other';
-    process.env.MAVEN_DISTRIBUTIONS_PATH = parentDirectory;
-    const expectedChildDirectory = directories[parentDirectory][0];
-
-    const mavenTarget = createMavenTarget();
-    const androidDisSpy = jest.spyOn(mavenTarget, 'getAndroidDistributionFile');
-    await mavenTarget.upload();
-
-    expect(androidDisSpy).toHaveBeenCalledTimes(1);
-    expect(androidDisSpy).toHaveBeenCalledWith(expectedChildDirectory);
-    expect(androidDisSpy).toHaveReturnedWith(Promise.resolve(undefined));
+  test('with only target options', () => {
+    expect(createMavenTarget).toThrowError(ConfigurationError);
   });
 });
 
