83d62ed50b94e9b6f0caf9c73dd2285fb74fc4a6
Burak Yigit Kaya <byk@sentry.io>
2021-10-05 20:08:28 +0300

    fix: Only log error on tempdir rm when ther is an error
    Fixes the useless and incorrect
    `Couldn't remote temp dir XXX: null` error.

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -90,7 +90,9 @@ export async function withTempDir<T>(
         // don't care as this is already a temporary file and will be removed
         // eventually by the OS. And it doesn't make sense to wait until this op
         // finishes then as nothing relies on the removal of this file.
-        logger.trace(`Couldn't remove temp dir ${directory}: `, err);
+        if (err) {
+          logger.trace(`Couldn't remove temp dir ${directory}:`, err);
+        }
       });
     }
   }
