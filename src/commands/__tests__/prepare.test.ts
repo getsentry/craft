3c71ac98415c6719c822d5787be154cc3838d783
Matt Gaunt <matt.gaunt@sentry.io>
2022-08-31 11:11:20 -0700

    Add additional context to error when version has a 'v' prefix

--- src/commands/__tests__/prepare.test.ts
+++ src/commands/__tests__/prepare.test.ts
@@ -1,6 +1,6 @@
 import { join as pathJoin } from 'path';
 import { spawnProcess } from '../../utils/system';
-import { runPreReleaseCommand } from '../prepare';
+import { runPreReleaseCommand, checkVersionOrPart } from '../prepare';
 
 jest.mock('../../utils/system');
 
@@ -51,5 +51,37 @@ describe('runPreReleaseCommand', () => {
         },
       }
     );
+  });
+
+  test('return true for valid version', () => {
+    const validVersions = [
+      '2.3.3',
+      '0.0.1',
+    ];
+    for (const v of validVersions) {
+      expect(
+        checkVersionOrPart({
+          newVersion: v,
+        }, null)
+      ).toBe(true);
+    }
+  });
+
+  test('throw an error for invalid version', () => {
+    const invalidVersions = [
+      { v: 'invalid-2.3.3', e: 'Invalid version or version part specified: "invalid-2.3.3"' },
+      { v: 'v2.3.3', e: 'Invalid version or version part specified: "v2.3.3". Removing the "v" prefix will likely fix the issue' },
+      { v: 'major', e: 'Version part is not supported yet' },
+      { v: 'minor', e: 'Version part is not supported yet' },
+      { v: 'patch', e: 'Version part is not supported yet' },
+    ];
+    for (const t of invalidVersions) {
+      const fn = () => {
+        checkVersionOrPart({
+          newVersion: t.v,
+        }, null);
+      };
+      expect(fn).toThrow(t.e);
+    }
   });
 });
