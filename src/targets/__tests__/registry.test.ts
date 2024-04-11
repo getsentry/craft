46f078836439947804264f0b7257db7e42abbc9e
Daniel Griesser <daniel.griesser.86@gmail.com>
2024-04-11 15:48:28 +0200

    Add mock

--- src/targets/__tests__/registry.test.ts
+++ src/targets/__tests__/registry.test.ts
@@ -1,8 +1,21 @@
+jest.mock('../../utils/githubApi.ts');
+import { getGitHubClient } from '../../utils/githubApi';
 import { RegistryConfig, RegistryTarget } from '../registry';
 import { NoneArtifactProvider } from '../../artifact_providers/none';
 import { RegistryPackageType } from '../../utils/registry';
 
 describe('getUpdatedManifest', () => {
+  let mockClient: jest.Mock;
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    mockClient = jest.fn();
+    (getGitHubClient as jest.MockedFunction<
+      typeof getGitHubClient
+      // @ts-ignore we only need to mock a subset
+    >).mockReturnValue({ graphql: mockClient });
+  });
+
   const target = new RegistryTarget(
     { name: 'pypi' },
     new NoneArtifactProvider(),
