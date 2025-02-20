fa99ff9048dd2860acf81c00475baa12197fe4e8
Jeffrey Hung <17494876+Jeffreyhung@users.noreply.github.com>
2025-02-20 13:32:30 -0800

    remove username and getauthuser from codes

--- src/targets/awsLambdaLayer.ts
+++ src/targets/awsLambdaLayer.ts
@@ -4,7 +4,6 @@ import * as path from 'path';
 import { Octokit } from '@octokit/rest';
 import simpleGit from 'simple-git';
 import {
-  getAuthUsername,
   getGitHubApiToken,
   getGitHubClient,
   GitHubRemote,
@@ -139,8 +138,7 @@ export class AwsLambdaLayerTarget extends BaseTarget {
     this.logger.trace('AWS regions: ', awsRegions);
 
     const remote = this.awsLambdaConfig.registryRemote;
-    const username = await getAuthUsername(this.github);
-    remote.setAuth(username, getGitHubApiToken());
+    remote.setAuth(getGitHubApiToken());
 
     await withTempDir(
       async directory => {
